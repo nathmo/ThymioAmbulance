67790219c210179381e64440d9b3a5ee9ea0c7bf
Nathann Morand <natmo@hotmail.ch>
2024-11-16 18:21:47 +0100

    housekeeping

--- pathplanner.py
+++ pathplanner.py
@@ -14,6 +14,10 @@ class PathPlanner:
         :param n: Minimum distance (in pixels) between points and obstacles.
         """
         self.pixel_size_mm = pixel_size_mm
+        # This approach ensure that the thymio will not hit any obstacle while not having to evaluate a complex
+        # bounding box. we take the small radius (with of the robot) along the line because the robot go straight
+        # we take the larger radius at each node as the robot will have to turn on itself at theses point and must
+        # avoid colliding into obstacle
         self.pointRadius = int(80/self.pixel_size_mm)  # minimum distance from other points and obstacles(~80mm)
         self.lineRadius = int(55/self.pixel_size_mm)  # minimum distance from other points and obstacles(~55mm)
 
